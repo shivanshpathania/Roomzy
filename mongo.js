0f911631dba51d86f739ae251a9cfb18f4a48d5e
shivanshpathania <shivanshpathania27@gmail.com>
2025-09-23 01:08:36 +0530

    Updated mongo.js

--- mongo.js
+++ mongo.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB=async ()=>{
     try{
         await mongoose.connect("mongodb+srv://ShivanshPathania:9gyroy9p8@cluster0.tlj0zgz.mongodb.net/")
+        
 
         console.log("Database Connected");
     } catch(err){
